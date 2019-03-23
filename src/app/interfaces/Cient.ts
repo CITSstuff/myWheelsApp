a629cbe124fb338ff2fa4bf27399cf5749d08acd
Ceeyar <201490428@student.uj.ac.za>
2019-03-23 19:40:57 +0200

    updated Client model

--- src/app/interfaces/Cient.ts
+++ src/app/interfaces/Cient.ts
@@ -1,7 +1,10 @@
 
 export interface Client{ 
   id: string,
+  id_number: number,
   first_name: string,
   last_name: string,
-  email: string    
+  token: number,
+  email: string, 
+  role: string
 } 
