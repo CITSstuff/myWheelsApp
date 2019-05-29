883090bcf153abb2939f07dac3737552325c9f63
CeeyarRbt <cndovela@retrorabbit.co.za>
2019-05-29 15:30:38 +0200

    created reservation info

--- src/app/views/reservation/reservation.component.ts
+++ src/app/views/reservation/reservation.component.ts
@@ -16,52 +16,21 @@ export interface Client {
 })
 
 export class ReservationComponent implements OnInit {
-    stateCtrl = new FormControl();
-    
-  /*clientCtrl = new FormControl();
-  filteredClients: Observable<Client[]>;
-
-  clients: Client[] = [
-    {
-      name: 'Arkansas',
-      tokens: 2,
-      dp: '../../../assets/img/noIMG.PNG'
-    },
-    {
-      name: 'California',
-      tokens: 3,
-      dp: '../../../assets/img/noIMG.PNG'
-    },
-    {
-      name: 'Florida',
-      tokens: 20,
-      dp: '../../../assets/img/noIMG.PNG'
-    },
-    {
-      name: 'Texas',
-      tokens: 27,
-      dp: '../../../assets/img/noIMG.PNG'
-    }
-  ];*/
-
-  constructor() {
-  }
-  minDate = new Date();
-  // maxDate = new Date(this.minDate.setDate(this.minDate.getDate() + 1))
-  public isChecked = false;
+ 
+  constructor() {}
+  stateCtrl = new FormControl();
   myControl = new FormControl();
   clientOptions: string[] = ['Olwethu', 'Ali', 'Nemza', 'Big client'];
   filteredOptions: Observable<string[]>;
 
 
   ngOnInit() {
-    this.isChecked = false;
+
     this.filteredOptions = this.myControl.valueChanges
       .pipe(
         startWith(''),
         map(value => this._filter(value))
       );
-    console.log("is ", this.stateCtrl.status);
 
   }
 
@@ -70,4 +39,5 @@ export class ReservationComponent implements OnInit {
 
     return this.clientOptions.filter(client => client.toLowerCase().includes(filterValue));
   }
+
 }
