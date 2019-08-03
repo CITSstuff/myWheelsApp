e0aa94cefe4bca6fdcd7d58b896d307364ce6b0e
siya_ndovela <siyabonga@sov.tech>
2019-08-03 22:15:23 +0200

    Updated the Fleet Data

--- src/app/interfaces/Fleet.ts
+++ src/app/interfaces/Fleet.ts
@@ -1,7 +1,12 @@
-
-  export interface Fleet {
-      id: string
-      avatar: string;
-      registration: string;
-      about: string
-    }
+export class Issue {
+    id: number;
+    index: number;
+    make: string;
+    date_added: string;
+    year: string;
+    color: string;
+    kms: string;
+    tank: string;
+    registration: string;
+    status: string
+  }
